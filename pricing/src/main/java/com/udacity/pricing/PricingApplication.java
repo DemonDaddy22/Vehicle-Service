85dbba7f98a50e35440e9160011a2d816d812dea
DemonDaddy22 <guptarohan369@gmail.com>
2020-10-17 11:01:39 +0530

    add eureka server and client

--- pricing/src/main/java/com/udacity/pricing/PricingApplication.java
+++ pricing/src/main/java/com/udacity/pricing/PricingApplication.java
@@ -2,8 +2,10 @@ package com.udacity.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PricingApplication {
 
 	public static void main(String[] args) {
